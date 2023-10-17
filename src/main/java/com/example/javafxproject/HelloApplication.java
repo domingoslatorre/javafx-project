47fac729ae9af02bb0d13448312cce4150303032
Domingos Latorre <domingos.latorre@gmail.com>
2023-10-17 10:40:24 -0300

    fxml exemplo 1

--- src/main/java/com/example/javafxproject/HelloApplication.java
+++ src/main/java/com/example/javafxproject/HelloApplication.java
@@ -1,12 +1,8 @@
 package com.example.javafxproject;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,20 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        VBox vBox = new VBox();
-        vBox.setPadding(new Insets(20, 20, 20, 20));
-        vBox.setSpacing(20);
-        vBox.setAlignment(Pos.CENTER);
-
-        Label lblMensagem = new Label("...");
-        Button btnVerMensagem = new Button("Ver mensagem");
-
-        btnVerMensagem.setOnAction((e) -> {
-            lblMensagem.setText("Olá mundo!!!");
-        });
-
-        vBox.getChildren().setAll(lblMensagem, btnVerMensagem);
-        Scene scene = new Scene(vBox);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.setTitle("Aplicação");
         stage.sizeToScene();
